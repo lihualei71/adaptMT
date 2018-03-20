a1dfcde0c9e76df5ecc377d2be2f12b168ee68c0
lihualei71 <lihua.lei@berkeley.edu>
2018-03-20 10:57:30 -0700

    split adapt_1d_plot into plot_thresh_1d and plot_lfdr_1d

--- R/adapt-plot.R
+++ R/adapt-plot.R
@@ -1,15 +1,13 @@
 #' Plotting
 #' 
 #' @export
-adapt_1d_plot <- function(obj, alpha, title,
-                          data = NULL,
-                          xlab = "x",
-                          disp_pmax = 0.2,
-                          disp_lfdrmax = 0.2,
-                          num_xbreaks = 3,
-                          rand_seed_perturb = NA,
-                          legend_pos = "topright",
-                          ...){
+plot_thresh_1d <- function(obj, alpha, title,
+                           data = NULL,
+                           xlab = "x",
+                           disp_ymax = 0.2,
+                           num_xbreaks = 3,
+                           rand_seed_perturb = NA,
+                           ...){
     if (!"adapt" %in% class(obj)){
         stop("obj is not an 'adapt' object.")
     }
@@ -28,33 +26,24 @@ adapt_1d_plot <- function(obj, alpha, title,
         ind <- max(which(alphas <= alpha))
         s <- obj$s[, ind]
     }
-    params_alphas <- sapply(obj$params, function(x){x$alpha})
-    if (alpha > max(params_alphas)){
-        ind <- 1
-    } else {
-        ind <- min(which(params_alphas >= alpha))
-    }
-    pix <- obj$params[[ind]]$pix
-    mux <- obj$params[[ind]]$mux
 
     x_ord <- order(x)
     pvals <- pvals[x_ord]
     s <- s[x_ord]
-    pix <- pix[x_ord]
-    mux <- mux[x_ord]
     x <- x[x_ord]
-    
-    par(mfrow = c(2, 1), ...)
+
+    par(...)
+    ## par(mfrow = c(2, 1), ...)
 
     ## Top panel on threshold curves
     what_type <- ifelse(pvals < s, 1, ifelse(pvals > 1 - s, 2, 3))
-    plot(x, (1:n * disp_pmax) / n, type = "n", pch = ".",
+    plot(x, (1:n * disp_ymax) / n, type = "n", pch = ".",
          xaxs = "i", yaxs = "i", ylab = "p-values", xlab = "",
          col = c("red", "blue", "black")[what_type], yaxt = "n",
          main = paste("Rejection threshold (", title, ")",
              sep = ""))
-    axis(2, at = seq(0, disp_pmax, length.out = num_xbreaks),
-         labels = seq(0, disp_pmax, length.out = num_xbreaks))
+    axis(2, at = seq(0, disp_ymax, length.out = num_xbreaks),
+         labels = seq(0, disp_ymax, length.out = num_xbreaks))
     hhi <- hlo <- .5
     vlo <- -.01
     vhi <- 1.01
@@ -71,30 +60,61 @@ adapt_1d_plot <- function(obj, alpha, title,
     points(x, pvals, pch = ".",
            col = c("red", "blue", "black")[what_type])
     box()
+}
 
+plot_lfdr_1d <- function(obj, alpha, title,
+                         data = NULL,
+                         xlab = "x",
+                         disp_ymax = 0.2,
+                         num_xbreaks = 3,
+                         legend_pos = "topright",
+                         ...){
+    if (!"adapt" %in% class(obj)){
+        stop("obj is not an 'adapt' object.")
+    }
+
+    if (is.null(data)){
+        data <- obj$data
+    }
+    x <- as.numeric(data[["x"]][,1])
+    pvals <- data[["pvals"]]
+    n <- length(pvals)
+    dist <- obj$dist
+    params_alphas <- sapply(obj$params, function(x){x$alpha})
+    if (alpha > max(params_alphas)){
+        ind <- 1
+    } else {
+        ind <- min(which(params_alphas >= alpha))
+    }
+    pix <- obj$params[[ind]]$pix
+    mux <- obj$params[[ind]]$mux
+
+    x_ord <- order(x)
+    pvals <- pvals[x_ord]
+    pix <- pix[x_ord]
+    mux <- mux[x_ord]
+    x <- x[x_ord]
+
+    par(...)
+    
     ## Bottom panel on local fdr level curves.
-    ind <- order(x)
-    x <- x[ind]
-    mux <- mux[ind]
-    pix <- pix[ind]    
-
     x_grid <- c(seq(1, n, by = 100))
     if (x_grid[length(x_grid)] < n){
         x_grid <- c(x_grid, n)
     }
-    p_grid <- exp(seq(-15, log(disp_lfdrmax), length.out=100))
+    p_grid <- exp(seq(-15, log(disp_ymax), length.out=100))
     xp_grid <- expand.grid(x = x_grid, p = p_grid)
     locfdr_vals <-
         compute_lfdr_mix(p = xp_grid$p, dist = dist,
                          params = list(mux = mux[xp_grid$x],
                              pix = pix[xp_grid$x]))
     locfdr_mat <- matrix(locfdr_vals, nrow = length(x_grid))
-    plot(0, 0, xlim = c(min(x), max(x)), ylim = c(0, disp_lfdrmax),
+    plot(0, 0, xlim = c(min(x), max(x)), ylim = c(0, disp_ymax),
          type = "n", xaxs = "i", yaxs = "i", yaxt = "n", 
          main = paste("Estimated local FDR (", title, ")", sep=""),
          ylab = "p-value", xlab = xlab)
-    axis(2, at = seq(0, disp_lfdrmax, length.out = num_xbreaks),
-         labels = seq(0, disp_lfdrmax, length.out = num_xbreaks))
+    axis(2, at = seq(0, disp_ymax, length.out = num_xbreaks),
+         labels = seq(0, disp_ymax, length.out = num_xbreaks))
     colors <- c("#CB181D", "#FB6A4A", "#FCAE91", "#FEE5D9", "white")
     .filled.contour(x = x[x_grid] + 1e-10 * (1:length(x_grid)),
                     y = p_grid, z = locfdr_mat,

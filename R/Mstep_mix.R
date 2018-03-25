ef8cebcfb9a3c7ab6bbd8d5cd5df7eaab1df6c0b
lihualei71 <lihua.lei@berkeley.edu>
2018-03-24 23:17:52 -0700

    slight modification of Mstep_mix.R: remove Hhat from weights in fitting mu(x)

--- R/Mstep_mix.R
+++ R/Mstep_mix.R
@@ -13,13 +13,14 @@ Mstep_mix <- function(x, pvals, dist,
     n <- length(Hhat)
     x_aug <- rbind(x, x)
     H_aug <- c(rep(1, n), rep(0, n))
-    weights <- c(Hhat, 1 - Hhat) 
+    weights <- c(Hhat, 1 - Hhat)
     piargs <- complete_args(x_aug, H_aug, pifun, piargs, weights)
     pi_res <- fit_pi(pifun, piargs, type = "Mstep")
     pi_res$pix <- pi_res$pix[1:n]
 
     y_aug <- c(dist$g(pvals), dist$g(1 - pvals))
-    weights <- c(Hhat * bhat, Hhat * (1 - bhat))
+    ## weights <- c(Hhat * bhat, Hhat * (1 - bhat))
+    weights <- c(bhat, 1 - bhat)
     muargs <- complete_args(x_aug, y_aug, mufun, muargs, weights)
     mu_res <- fit_mu(mufun, muargs, dist, type = "Mstep")
     mu_res$mux <- mu_res$mux[1:n]

8eecfb9c127db82a8a13f6ee52f6f808ef32b64a
lihualei71 <lihua.lei@berkeley.edu>
2018-07-21 15:25:22 -0700

    update fit_pi for the corner case

--- R/fit-helpers.R
+++ R/fit-helpers.R
@@ -8,12 +8,15 @@ fit_pi <- function(fun, args, type = c("Mstep", "init")){
     } else if (type == "init"){
         fun_name <- "\'pifun_init\'"
     }
-    
+
     if (is.null(args)) {
         stop(paste0(fun_name, " has irregular input types. Replace it with another function or writing a wrapper of ", fun_name, " with regular types of input (formula = , data = ) or (x = x, y = y) or (X = x, y = y)"))
     }
 
-    fit <- do.call(fun, args)
+    fit <- try(do.call(fun, args))
+    if (class(fit) == "try-error"){
+        warning("Initialization of pi(x) by \'pifun_init\' fails. Initialize pi(x) as a constant 0.1 by default. Please check if too few p-values lying in [s0, 1-s0] and decrease s0 or change \'pifun_init\' if so. ")
+    }
     if (is.null(fit$fitv)){
         stop(paste0(fun_name, " does not output \'fitv\'. Replace it with another function or change the name for fitted value to \'fitv\'"))
     }
